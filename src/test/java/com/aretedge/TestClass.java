222617d410fdeac9a0f7bdbbc10eac6a801b3b0a
Gourav Das <gourav.das@aretedge.com>
2024-11-21 10:48:27 +0530

    3rd push

---
+++ src/test/java/com/aretedge/TestClass.java
@@ -0,0 +1,12 @@
+package com.aretedge;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public static void test(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
